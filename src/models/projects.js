c543314b89ca21aa9e60d8e01960ff4565dde96a
slplaine <simoneleca@gmail.com>
2026-07-16 08:46:21 -0300

    making improvements

--- src/models/projects.js
+++ src/models/projects.js
@@ -2,7 +2,7 @@ import db from './db.js';
 
 export const getAllProjects = async () => {
   const query = `
-    SELECT p.project_id, p.title, p.date, o.name AS organization_name
+    SELECT p.project_id, p.title, p.location, p.date, o.name AS organization_name
     FROM projects p
     JOIN organization o ON p.organization_id = o.organization_id
     ORDER BY p.date;
