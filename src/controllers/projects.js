3e97b535aabe8668357b4fad0600b9314887eab9
slplaine <simoneleca@gmail.com>
2026-08-12 18:07:38 -0300

    test error

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -1,16 +1,36 @@
 import { body, validationResult } from 'express-validator';
 import { getUpcomingProjects,getProjectDetails} from '../models/projects.js';
 import { getCategoriesByProjectId } from '../models/categories.js';
+import { createProject } from '../models/projects.js';
+import { getAllOrganizations } from '../models/organizations.js';
 
 
 const NUMBER_OF_UPCOMING_PROJECTS = 5
-
+/*
 // Define any controller functions
 const showProjectsPage = async (req, res) => {
     const projects = await getUpcomingProjects(NUMBER_OF_UPCOMING_PROJECTS);
     const title = 'Upcoming Service Projects';
 
     res.render('projects', {title, projects });
+};
+*/
+const showProjectsPage = async (req, res) => {
+
+    const projects = [
+        {
+            project_id: 1,
+            title: 'TESTE',
+            organization_id: 1,
+            organization_name: 'TESTE ORG',
+            location: 'JOINVILLE',
+            date: new Date()
+        }
+    ];
+
+    const title = 'Upcoming Service Projects';
+
+    res.render('projects', { title, projects });
 };
 
 const showProjectDetailsPage = async (req, res) => {
