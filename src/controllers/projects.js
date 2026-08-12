fe5bb927dfd21ce8251efac54bb1badbcd014305
slplaine <simoneleca@gmail.com>
2026-08-12 17:21:21 -0300

    making improvements

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -20,9 +20,34 @@ const showProjectDetailsPage = async (req, res) => {
 
   res.render('project', { title: project.title, project, categories });
 };
+const showNewProjectForm = async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Add New Service Project';
+
+    res.render('new-project', { title, organizations });
+}
+
+const processNewProjectForm = async (req, res) => {
+    // Extract form data from req.body
+    const { title, description, location, date, organizationId } = req.body;
+
+    try {
+        // Create the new project in the database
+        const newProjectId = await createProject(title, description, location, date, organizationId);
+
+        req.flash('success', 'New service project created successfully!');
+        res.redirect(`/project/${newProjectId}`);
+    } catch (error) {
+        console.error('Error creating new project:', error);
+        req.flash('error', 'There was an error creating the service project.');
+        res.redirect('/new-project');
+    }
+}
 
 // Export any controller functions
 export {
   showProjectsPage,
-  showProjectDetailsPage
+  showProjectDetailsPage,
+  showNewProjectForm,
+  processNewProjectForm
 };
--- src/routes.js
+++ src/routes.js
@@ -34,11 +34,15 @@ router.get('/category/:id', showCategoryDetailsPage);
 router.get('/new-organization', showNewOrganizationForm);
 // Route to display the edit organization form
 router.get('/edit-organization/:id', showEditOrganizationForm);
+// Route for new project page
+router.get('/new-project', showNewProjectForm);
 
 // Route to handle new organization form submission
 router.post('/new-organization', organizationValidation, processNewOrganizationForm);
 // Route to handle the edit organization form submission
 router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
+// Route to handle new project form submission
+router.post('/new-project', processNewProjectForm);
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
