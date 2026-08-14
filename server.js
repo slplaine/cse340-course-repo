71c0b42c20e5ed7db8da53e63dfe2ea442b2f280
slplaine <simoneleca@gmail.com>
2026-08-14 17:04:51 -0300

    making activity

--- server.js
+++ server.js
@@ -52,15 +52,17 @@ app.use((req, res, next) => {
   next();
 });
 
-
-// Middleware to make NODE_ENV available to all templates
+// Middleware to set res.locals variables for to all templates
 app.use((req, res, next) => {
-  res.locals.isLoggedIn = false;
+    res.locals.isLoggedIn = false;
     if (req.session && req.session.user) {
         res.locals.isLoggedIn = true;
     }
-  res.locals.NODE_ENV = NODE_ENV;
-  next();
+
+    res.locals.user = req.session.user || null;
+
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
 });
 
 // Use the imported router to handle routes
--- src/routes.js
+++ src/routes.js
@@ -35,7 +35,8 @@ import {
     processLoginForm,
     processLogout,
     requireLogin,
-    showDashboard
+    showDashboard,
+    requireRole
 } from './controllers/users.js'; 
 import { showHomePage } from './controllers/index.js';
 import { testErrorPage } from './controllers/errors.js';
@@ -55,33 +56,33 @@ router.get('/project/:id', showProjectDetailsPage);
 // Route for category details page
 router.get('/category/:id', showCategoryDetailsPage);
 // Route for new organization form
-router.get('/new-organization', showNewOrganizationForm);
+router.get('/new-organization', requireRole('admin'), showNewOrganizationForm);
 // Route to display the edit organization form
-router.get('/edit-organization/:id', showEditOrganizationForm);
+router.get('/edit-organization/:id', requireRole('admin'), showEditOrganizationForm);
 // Route for new project page
-router.get('/new-project', showNewProjectForm);
+router.get('/new-project', requireRole('admin'), showNewProjectForm);
 
 // Route to handle new organization form submission
-router.post('/new-organization', organizationValidation, processNewOrganizationForm);
+router.post('/new-organization', requireRole('admin'), organizationValidation, processNewOrganizationForm);
 // Route to handle the edit organization form submission
-router.post('/edit-organization/:id', organizationValidation, processEditOrganizationForm);
+router.post('/edit-organization/:id', requireRole('admin'), organizationValidation, processEditOrganizationForm);
 // Route to handle new project form submission
-router.post('/new-project',projectValidation, processNewProjectForm);
+router.post('/new-project', requireRole('admin'), projectValidation, processNewProjectForm);
 // Route to display the edit project form
-router.get('/edit-project/:id', showEditProjectForm);
+router.get('/edit-project/:id', requireRole('admin'),  showEditProjectForm);
 // Route to handle the edit project form submission
-router.post('/edit-project/:id',projectValidation,processEditProjectForm);
+router.post('/edit-project/:id', requireRole('admin'), projectValidation, processEditProjectForm);
 // Routes to handle the assign categories to project form
-router.get('/assign-categories/:projectId', showAssignCategoriesForm);
-router.post('/assign-categories/:projectId', processAssignCategoriesForm);
+router.get('/assign-categories/:projectId', requireRole('admin'), showAssignCategoriesForm);
+router.post('/assign-categories/:projectId', requireRole('admin'), processAssignCategoriesForm);
 // Route for new category page
-router.get('/new-category', showNewCategoryForm);
+router.get('/new-category', requireRole('admin'), showNewCategoryForm);
 // Route to handle new category form submission
-router.post('/new-category',categoryValidation,processNewCategoryForm);
+router.post('/new-category', requireRole('admin'), categoryValidation,processNewCategoryForm);
 // Route to display the edit category form
-router.get('/edit-category/:id', showEditCategoryForm);
+router.get('/edit-category/:id', requireRole('admin'), showEditCategoryForm);
 // Route to handle the edit category form submission
-router.post('/edit-category/:id',categoryValidation,processEditCategoryForm);
+router.post('/edit-category/:id', requireRole('admin'), categoryValidation,processEditCategoryForm);
 
 // User registration routes
 router.get('/register', showUserRegistrationForm);
